f31b4046dd5e3fa605ebc687b4aaedc415e1533e
Mrdrra_Mint <srkyman@gmail.com>
2018-01-27 19:10:21 +0100

    ispravka jojooo

--- SoulTransmitter/GGJ2018/Assets/Scripts/MapManager/Map.cs
+++ SoulTransmitter/GGJ2018/Assets/Scripts/MapManager/Map.cs
@@ -38,8 +38,8 @@ public class Map : MonoBehaviour {
         foreach(Room room in rooms)
             foreach (Room node in rooms)
                 for (int i = 0; i < 4; i++)
-                    if (node.transform.position.x - room.x + Room.XSize * Room.DirectionVectors[i].x < 0.005 &&
-                        node.transform.position.y - room.y + Room.YSize * Room.DirectionVectors[i].y < 0.005)
+					if (node.transform.position.x - room.transform.position.x + Room.XSize * Room.DirectionVectors[i].x < 0.005 &&
+						node.transform.position.y - room.transform.position.y + Room.YSize * Room.DirectionVectors[i].y < 0.005)
                         room.nodes[i] = node;
             
     }
