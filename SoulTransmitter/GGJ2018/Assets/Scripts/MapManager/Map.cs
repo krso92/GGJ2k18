37f3fa4c7ed9689c7496fa9bf1a3932bd2a740cc
Demvamko <demvamko@gmail.com>
2018-01-27 18:28:19 +0100

    Spawn deleted

--- SoulTransmitter/GGJ2018/Assets/Scripts/MapManager/Map.cs
+++ SoulTransmitter/GGJ2018/Assets/Scripts/MapManager/Map.cs
@@ -4,10 +4,6 @@ using UnityEngine;
 using System;
 
 public class Map : MonoBehaviour {
-    public const string ROOM_NAME = "room_";
-
-    public TextAsset mapFile;
-
     [HideInInspector]
 	public Room startRoom;
     [HideInInspector]
@@ -37,34 +33,4 @@ public class Map : MonoBehaviour {
 	void Init(){
 		
 	}
-
-    Room Spawn(int xStart, int yStart){
-        string fullMap = mapFile.text;
-
-        string[] lines = fullMap.Split('\n');
-
-		Room spawn = null;
-
-        for (int x = 0; x < lines.Length; x++)
-            for (int y = 0; y < lines.Length; y++){
-                GameObject prefab = Resources.Load<GameObject>(ROOM_NAME + lines[y][x]);
-
-                GameObject roomGO = GameObject.Instantiate(prefab, new Vector3(-x * Room.XSize, y * Room.YSize), Quaternion.identity);
-                Room room = roomGO.GetComponent<Room>();
-
-                if(!room){
-                    Debug.LogError("Neko bre zaboravio skriptu room stavit na sobu");
-                    throw new Exception("Invalid Prefab");
-                }
-
-                if (xStart == x && yStart == y)
-                    spawn = room;
-
-                rooms.Add(room);
-            }
-
-        return spawn;
-    }
-
-
 }
