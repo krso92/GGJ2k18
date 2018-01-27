4f17439429272260a01be1b4a790e229d586af54
Mrdrra_Mint <srkyman@gmail.com>
2018-01-27 15:51:23 +0100

    Init prefab placeholder + Map manager

---
+++ SoulTransmitter/GGJ2018/Assets/Scripts/MapManager/Map.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour {
+
+	public Room startRoom;
+	public Room exitRoom;
+
+	private List<Room> rooms;
+
+	public delegate void InitFinished();
+
+	public event InitFinished OnInitFinished;
+
+	// Use this for initialization
+	void Start () {
+		startRoom.OnRoomEnter.Invoke ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// mozda nesto
+	}
+
+
+	// 
+
+	void Init(){
+		
+	}
+
+}
---
+++ SoulTransmitter/GGJ2018/Assets/Scripts/MapManager/Room.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour {
+
+	public List<Enemy> enemies;
+
+	public delegate void RoomEnter ();
+	public delegate void RoomExit ();
+
+	public event RoomEnter OnRoomEnter = delegate {};
+	public event RoomExit OnRoomExit = delegate {};
+
+	// Use this for initialization
+	void Start () {
+			
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// Spawnuje sve redom
+	void Spawn(){
+		
+	}
+}
