57885a34334b5112ef525932b7c1b59251555bff
Mrdrra_Mint <srkyman@gmail.com>
2018-01-28 18:59:03 +0100

    baba boss animation + shiznit

---
+++ SoulTransmitter/GGJ2018/Assets/BabaAnimHelp.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BabaAnimHelp : MonoBehaviour {
+
+	public Baba baba;
+
+	public void Shoot() {
+		baba.PerformAttackAction ();	
+	}
+}
